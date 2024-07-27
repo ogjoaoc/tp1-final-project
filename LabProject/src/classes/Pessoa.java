d5c6dedfd4a2babc60d6d08b0ca471c6ede6be41
Arthur <arthur.bispo4@gmail.com>
2024-07-27 20:16:32 -0300

    Add Pessoa e subclasses

--- LabProject/src/classes/Pessoa.java
+++ LabProject/src/classes/Pessoa.java
@@ -1,13 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package classes;
 
-/**
- *
- * @author analuisa
- */
 public class Pessoa {
+    protected String nome;
+    protected String cpf;
+    protected String sexo;
+    protected String dataNascimento;
+    protected String email;
+
+    public Pessoa(String nome, String cpf, String sexo, String dataNascimento, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.sexo = sexo;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
     
 }
