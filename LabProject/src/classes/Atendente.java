25437bdeeff34dd76d50f355c4c94beaa9ae92cb
ogjoaoc <joaocarlosgoncalvesfilho@gmail.com>
2024-07-28 22:35:35 -0300

    update: banco de dados

--- LabProject/src/classes/Atendente.java
+++ LabProject/src/classes/Atendente.java
@@ -4,12 +4,14 @@ public class Atendente extends Pessoa {
     private String turno;
     private int credencial;
     private int qtdAgendamentos;
+    protected String senha;
 
-    public Atendente(String turno, int credencial, int qtdAgendamentos, String nome, String cpf, String sexo, String dataNascimento, String email) {
+    public Atendente(String turno, int credencial, int qtdAgendamentos, String senha, String nome, String cpf, String sexo, String dataNascimento, String email) {
         super(nome, cpf, sexo, dataNascimento, email);
         this.turno = turno;
         this.credencial = credencial;
         this.qtdAgendamentos = qtdAgendamentos;
+        this.senha = senha;
     }
 
     public String getTurno() {
@@ -35,6 +37,56 @@ public class Atendente extends Pessoa {
     public void setQtdAgendamentos(int qtdAgendamentos) {
         this.qtdAgendamentos = qtdAgendamentos;
     }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
     
     
 }
--- LabProject/src/classes/Enfermeiro.java
+++ LabProject/src/classes/Enfermeiro.java
@@ -2,10 +2,12 @@ package classes;
 
 public class Enfermeiro extends Pessoa {    
     private boolean disponivel;
+    protected String senha;
 
-    public Enfermeiro(boolean disponivel, String nome, String cpf, String sexo, String dataNascimento, String email) {
+    public Enfermeiro(boolean disponivel, String senha, String nome, String cpf, String sexo, String dataNascimento, String email) {
         super(nome, cpf, sexo, dataNascimento, email);
         this.disponivel = disponivel;
+        this.senha = senha;
     }
 
     public boolean isDisponivel() {
@@ -15,4 +17,54 @@ public class Enfermeiro extends Pessoa {
     public void setDisponivel(boolean disponivel) {
         this.disponivel = disponivel;
     }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
 }
